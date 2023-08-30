2714777aa8631bb8152ed5e5627871dc9a0b7dfe
vuongNguyenVg <vuongnguyen332003@gmail.com>
2023-08-30 23:02:15 +0700

    my_app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+import {LogoutComponent} from "./logout/logout.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {EditProfileComponent} from "./edit-profile/edit-profile.component";
+import {ViewPostComponent} from "./view-post/view-post.component";
+import {NotificationComponent} from "./notification/notification.component";
+import {UserPageComponent} from "./user-page/user-page.component";
+import {ChangePasswordComponent} from "./change-password/change-password.component";
+import {FriendRequestComponent} from "./friend-request/friend-request.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' , component : HomeComponent},
+  {path: 'register' , component: RegisterComponent},
+  {
+    path: 'login' ,
+    component : LoginComponent
+  },
+  {
+    path : 'logout',
+    component : LogoutComponent
+  },
+  {
+    path: 'profile',
+    component : ProfileComponent
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfileComponent
+  },
+  {
+    path: 'view-post/:postId',
+    component: ViewPostComponent
+  },
+  {
+    path: 'notification',
+    component: NotificationComponent
+  },
+  {
+    path: 'user_page/:userName',
+    component: UserPageComponent
+  },
+  {
+    path: 'fakebook/update',
+    component: ChangePasswordComponent
+  },
+  {
+    path : 'friend_request',
+    component : FriendRequestComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { NotificationComponent } from './notification/notification.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { FriendRequestComponent } from './friend-request/friend-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LogoutComponent,
+    LoginComponent,
+    ProfileComponent,
+    EditProfileComponent,
+    ViewPostComponent,
+    NotificationComponent,
+    UserPageComponent,
+    ChangePasswordComponent,
+    FriendRequestComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
